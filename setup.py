1d42779681e0fbf8fbbabf7d2e3d7c4fcbde8713
Fabian Pedregosa <fabian@fseoane.net>
2013-05-10 14:39:05 +0200

    version 0.2

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ import numpy as np
 sources =['pysofia/_sofia_ml.pyx'] + glob.glob('pysofia/src/*.cc')
 
 setup(name='pysofia',
-    version='0.1',
+    version='0.2',
     description='Python bindings for sofia-ml',
     author='Fabian Pedregosa',
     author_email='fabian@fseoane.net',
     url='',
-    packages=['minirank'],
+    packages=['pysofia'],
     cmdclass = {'build_ext': build_ext},
     ext_modules = [Extension('pysofia._sofia_ml',
         sources=sources,
